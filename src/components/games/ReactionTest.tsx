ca29b12de2de175a93449a3446a1cd831e791b6a
G Vinith Sai Reddy <148205482+Vinithsaireddy@users.noreply.github.com>
2025-04-25 14:07:17 +0530

    Update ReactionTest.tsx

--- src/components/games/ReactionTest.tsx
+++ src/components/games/ReactionTest.tsx
@@ -19,7 +19,7 @@ const ReactionTest: React.FC<ReactionTestProps> = ({ onGameWin, onGameRestart })
 
   // Required attempts and target time to win
   const requiredAttempts = 5;
-  const targetTime = 70; // milliseconds
+  const targetTime = 200; // milliseconds
 
   // Start game
   const startGame = () => {
